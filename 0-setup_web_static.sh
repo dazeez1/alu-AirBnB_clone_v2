7342fb999c4458a2408290a38d04894d66ce4315
PMayala <p.mayala@alustudent.com>
2024-05-30 06:28:53 -0700

    Removed double backslashes before location in the sed command to ensure proper indentation in the inserted line. File: 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,14 +4,20 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
-# create folders
+# Create folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-#create a fake html file
+
+# Create a fake HTML file
 echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-# create symbolic link
+
+# Create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# give ownership
+# Give ownership
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+
+# Modify Nginx configuration
+sudo sed -i '38i\        location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
