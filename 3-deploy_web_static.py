bc17923e18d41b9d7b48364761eb397c6a1d91eb
dazeez1 <d.azeez@alustudent.com>
2024-06-02 22:42:18 -0700

    project

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Function that deploys """
 from datetime import datetime
-from fabric.api import env, local, put, run
+from fabric.api import *
 import os
+import shlex
+
 
 env.hosts = ['18.234.81.62', '34.230.70.28']
 env.user = "ubuntu"
@@ -12,12 +14,10 @@ def deploy():
     """ DEPLOYS """
     try:
         archive_path = do_pack()
-        if not archive_path:
-            return False
-        return do_deploy(archive_path)
-    except Exception as e:
-        print(f"An error occurred during deployment: {e}")
+    except:
         return False
+
+    return do_deploy(archive_path)
 
 
 def do_pack():
@@ -29,23 +29,27 @@ def do_pack():
         archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
         local('tar -cvzf {} web_static'.format(archive_path))
         return archive_path
-    except Exception as e:
-        print(f"An error occurred during packing: {e}")
+    except:
         return None
 
 
 def do_deploy(archive_path):
     """ Deploys """
     if not os.path.exists(archive_path):
-        print("Archive does not exist.")
         return False
     try:
-        name = os.path.basename(archive_path)
-        wname = os.path.splitext(name)[0]
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
         releases_path = "/data/web_static/releases/{}/".format(wname)
         tmp_path = "/tmp/{}".format(name)
 
-        put(archive_path, tmp_path)
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(releases_path))
         run("tar -xzf {} -C {}".format(tmp_path, releases_path))
         run("rm {}".format(tmp_path))
@@ -55,6 +59,5 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except Exception as e:
-        print(f"An error occurred during deployment: {e}")
+    except:
         return False
